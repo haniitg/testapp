236124d41f8020b301abaffdb163782d817851a2
hany karam <hkaram@egypt.itgsolutions.com>
2021-09-30 11:26:54 +0200

    tst

--- app 3/ViewController.swift
+++ app 3/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     var x = ""
+    var y = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
